b8ceaf18d6c2f1b95f46de058dc08caa69dfcd33
Oleg <danylo2008@users.noreply.github.com>
2014-06-15 16:22:22 +0300

    INIT COMMIT

---
+++ mike.ovseychuk/src/ua/artcode/week2/day2/ReflectionFileHelper/ClassInfo.java
@@ -0,0 +1,43 @@
+package ua.artcode.week2.day2.ReflectionFileHelper;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/**
+ * Created by okohanch on 15.06.2014.
+ */
+public class ClassInfo implements IClassInfo {
+
+    @Override
+    public String getClass(Object o) {
+        Class cl = o.getClass();
+        return cl.getName();
+    }
+
+    @Override
+    public String [] getFieldInfo(Object o) throws IllegalAccessException {
+        int index = 0;
+        String[] array = new String[100];
+        Class cl = o.getClass();
+        Field[] declaredFields = cl.getDeclaredFields();
+
+        for (Field f : declaredFields) {
+            int id = f.getModifiers();
+            String idName = Modifier.toString(id);
+            Class<?> type = f.getType();
+            String fieldTypeName = type.getName();
+            String fieldName = f.getName();
+            f.setAccessible(true);
+            Object value = f.get(o);
+            String field = generateString(idName, fieldTypeName, fieldName, value);
+            array[index] = field;
+            index++;
+
+
+        }
+        return array;
+    }
+        private String generateString(String idName, String fieldTypeName, String fieldName , Object o){
+        return idName + " " + fieldTypeName + " " + fieldName + " " + " = " + o.toString();
+    }
+}
