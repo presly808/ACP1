6ade1b5e58055b5384fd6bbeb538da568352a52a
VladimirBorisovich <kucheruk@ukr.net>
2014-06-15 16:31:39 +0300

    implementation ISever only method save

--- mike.ovseychuk/src/ua/artcode/week2/day2/ReflectionFileHelper/ISaver.java
+++ mike.ovseychuk/src/ua/artcode/week2/day2/ReflectionFileHelper/ISaver.java
@@ -1,11 +1,13 @@
 package ua.artcode.week2.day2.ReflectionFileHelper;
 
+import java.io.IOException;
+
 /**
  * Created by mike on 6/15/14.
  */
 public interface ISaver {
 
-    void save (Object o);
+    void save (Object o) throws IOException, IllegalAccessException;
     Object load();
 
 }
