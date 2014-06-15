5e376e0fa97e126bc17b166b3f3917acea9d1d45
Mike Ovseychuk <mikhailovseychuk@gmail.com>
2014-06-15 15:44:05 +0300

    create dir

--- mike.ovseychuk/src/ua/artcode/week2/day2/ReflectionFileHelper/ISaver.java
+++ mike.ovseychuk/src/ua/artcode/week2/day2/ReflectionFileHelper/ISaver.java
@@ -3,5 +3,9 @@ package ua.artcode.week2.day2.ReflectionFileHelper;
 /**
  * Created by mike on 6/15/14.
  */
-public class ISaver {
+public interface ISaver {
+
+    void save (Object o);
+    Object load();
+
 }
