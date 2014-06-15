7ca271a0734c9be10ff2ad435b641130600f2633
Mike Ovseychuk <mikhailovseychuk@gmail.com>
2014-06-15 15:41:38 +0300

    create dir

---
+++ mike.ovseychuk/src/ua/artcode/week2/day2/ReflectionFileHelper/IReflectParser.java
@@ -0,0 +1,9 @@
+package ua.artcode.week2.day2.ReflectionFileHelper;
+
+/**
+ * Created by mike on 6/15/14.
+ */
+public class IReflectParser {
+
+
+}
---
+++ mike.ovseychuk/src/ua/artcode/week2/day2/ReflectionFileHelper/ISaver.java
@@ -0,0 +1,7 @@
+package ua.artcode.week2.day2.ReflectionFileHelper;
+
+/**
+ * Created by mike on 6/15/14.
+ */
+public class ISaver {
+}
